646a63295d76d5eb9ef054e6b8bffcaf1d13f101
Puneet Uttam <puneet.uttam.99@gmail.com>
2024-08-15 19:16:20 +0530

    updates

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -8,12 +8,14 @@ export function Api(
     loanTypeCode,
     armType = "5-1"
 ) {
-    const url = `https://thingproxy.freeboard.io/fetch/https://www.consumerfinance.gov/oah-api/rates/rate-checker?price=${housePrice}&loan_amount=${loanAmt}&minfico=${creditScore}&maxfico=${creditScore + 19}&state=${stateCode}&rate_structure=${rateTypeCode}&loan_term=${loanTerm}&loan_type=${loanTypeCode}&arm_type=${armType}`;
+    const url = `https://thingproxy.freeboard.io/fetch/https://www.consumerfinance.gov/oah-api/rates/rate-checker?price=${housePrice}&loan_amount=${loanAmt}&minfico=${creditScore}&maxfico=${
+        creditScore + 19
+    }&state=${stateCode}&rate_structure=${rateTypeCode}&loan_term=${loanTerm}&loan_type=${loanTypeCode}&arm_type=${armType}`;
 
     return fetch(url)
         .then((res) => res.json())
         .catch((err) => {
             console.error(err);
-            return null;
+            return null; // Handle errors by returning null or fallback data
         });
 }
